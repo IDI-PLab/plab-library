38b7d359f2c93c622fc5b83e069d20bbfc3e196b
Sitron Te <halsaune@stud.ntnu.no>
2015-06-22 11:58:04 +0200

    Development of BTLE control in progress

--- PLabBT/PLabBTLEController.cpp
+++ PLabBT/PLabBTLEController.cpp
@@ -3,13 +3,13 @@
 class PLabBTLEDevice {
 friend class PLabBTLEController;
 private:
-    int id = -1;
-    char *name = 0;
-    char *address = 0;
+int id = -1;
+char *name = 0;
+char *address = 0;
 public:
-    int id() const;
-    const char *name() const;
-    const char *address() const;
+int id() const;
+const char *name() const;
+const char *address() const;
 }
 */
 
@@ -17,166 +17,279 @@ public:
 class PLabBTLEController : public SoftwareSerial
 {
 private:
-    enum State {
-        INIT, INIT_GETNAME, INIT_GETROLE, INIT_GETDISCNAME,
-	READY,
-	SETTING_ROLE, SETTING_NAME, SETTING_NAME_DISCOVERY,
-	DISCOVERY_RUNNING, CONNECT_RUNNING, DISCONNECTING,
-	RESET_IN_PROGRESS, FACTORY_RESET_IN_PROGRESS
-    };
+enum State {
+INIT, INIT_GETNAME, INIT_GETROLE, INIT_GETDISCNAME,
+READY,
+SETTING_ROLE, SETTING_NAME, SETTING_NAME_DISCOVERY,
+DISCOVERY_RUNNING, CONNECT_RUNNING, DISCONNECTING,
+RESET_IN_PROGRESS, FACTORY_RESET_IN_PROGRESS
+};
 
-    state = State::INIT;
+state = State::INIT;
 
-    char buffer[PLAB_BTLE_BSIZE];
-    int bLoc = 0;
+char buffer[PLAB_BTLE_BSIZE];
+int bLoc = 0;
 
-    char *name;
-    PLabBTLEDevice *devices;
-    bool connected, discoverNames;
-    Role role = Role::UNDEFINED;
+char *name;
+PLabBTLEDevice *devices;
+bool connected, discoverNames;
+Role role = Role::UNDEFINED;
 public:
 
-    enum Role {UNDEFINED=0, PERIPHERAL='0', CENTRAL='1'};
+enum Role {UNDEFINED=0, PERIPHERAL='0', CENTRAL='1'};
 
 }
 */
 // #define PLAB_BTLE_BSIZE 25
 void PLabBTLEController::init(long speed)
 {
-    begin(speed);
-    state = State::INIT;
-    bLoc = 0;
-    name = 0;
-    role = Role::UNDEFINED;
-    devices = new PLabBTLEDevice[PLAB_BTLE_MAXDEVICE];
-    for (int i = 0; i < PLAB_BTLE_BSIZE; ++i) buffer[i] = 0;
+	begin(speed);
+	state = State::INIT;
+	bLoc = 0;
+	name = 0;
+	role = Role::UNDEFINED;
+	_devices = new PLabBTLEDevice[PLAB_BTLE_MAXDEVICE];
+	for (int i = 0; i < PLAB_BTLE_BSIZE; ++i) buffer[i] = 0;
 }
 bool PLabBTLEController::readBuffer()
 {
-    if (SoftwareSerial::available() && bLoc < (PLAB_BTLE_BSIZE - 1))
-    {
-        buffer[bLoc++] = read();
-	buffer[bLoc] = 0;
-    }
-    return bLoc < PLAB_BTLE_BSIZE;
+	if (SoftwareSerial::available() && bLoc < (PLAB_BTLE_BSIZE - 1))
+	{
+		buffer[bLoc++] = read();
+		buffer[bLoc] = 0;
+	}
+	return bLoc < PLAB_BTLE_BSIZE;
 }
 const PLabBTLEController::PLabBTLEDevice &operator[](const int &index) const
 {
-    return devices[index];
+	return _devices[index];
+}
+
+void PLabBTLEController::updateName()
+{
+	delete[] name;
+	int size = engine.update(0);
+	name = new char[size + 1];
+	for (int i = 0; i <= size; i++)
+	{
+		name[i] = engine.getMessage()[i];
+	}
 }
 
 void PLabBTLEController::update()
 {
-    if (state == State::READY) return;
-    unsigned long now = millis();
-    bool overTime = (now - lastTime) >= PLAB_BTLE_AT_PAUSE;
-    lastTime = now;
+	if (state == State::READY) return;
+	unsigned long now = millis();
+	bool overTime = (now - lastTime) >= PLAB_BTLE_AT_PAUSE;
+	lastTime = now;
 
-    switch(state)
-    {
-    case State::INIT:
-        write("AT+NAME?");
-	state = State::INIT_GETNAME;
-        break;
-    case State::INIT_GETNAME:
-        if (overTime)
+	switch (state)
 	{
-	    delete[] name;
-	    name = new char[bLoc + 1];
-	    for (int i = 0; i < bLoc; i++)
-	    {
-	        name[i] = buffer[i];
-	    }
-	    name[bLoc] = 0;
-	    bLoc = 0;
-	    buffer[0] = 0;
-	    state = State::INIT_GETROLE;
-	    write("AT+ROLE?");
-	} else
-	{
-	    readBuffer();
-	    // If we have filled buffer with OK+NAME: we can discard it
-	    if (bLoc == 8 && buffer[bLoc-1] == ':')
-	    {
-	        if (buffer[0] == 'O' && buffer[1] == 'K' && buffer[2] == '+'
-		  && buffer[3] == 'N' && buffer[4] == 'A' && buffer[5] == 'M'
-		  && buffer[6] == 'E')
-		    bLoc = 0;
-	    }
+	case State::INIT:
+		state = State::INIT_GETNAME;
+		write("AT+NAME?");
+		engine.prefix = statePrefixes[statePrefixIndex[state]];
+		break;
+	case State::INIT_GETNAME:
+		if (overTime)
+		{
+			updateName();
+			state = State::INIT_GETROLE;
+			write("AT+ROLE?");
+			engine.prefix = statePrefixes[statePrefixIndex[state]];
+		}
+		else			if (SoftwareSerial::available())
+			engine.update(read());
+		break;
+	case State::INIT_GETROLE:
+		if (SoftwareSerial::available())
+		{
+			if (engine.update(read()) > 0)
+			{
+				engine.update(0);
+				char r = engine.getMessage()[0];
+				if (r == '0') role = Role.PERIPHERAL;
+				else if (r == '1') role = Role.CENTRAL;
+				else role = Role.UNDEFINED;
+				state = State::INIT_GETDISCNAME;
+				write("AT+SHOW?");
+				engine.prefix = statePrefixes[statePrefixIndex[state]];
+			}
+		}
+		break;
+	case State::INIT_GETDISCNAME:
+		if (SoftwareSerial::available())
+		{
+			if (engine.update(read()) > 0)
+			{
+				engine.update(0);
+				char r = engine.getMessage()[0];
+				discoverNames = r == '1';
+				state = State::INIT_WORKIMM;
+				write("AT+IMME?");
+				engine.prefix = statePrefixes[statePrefixIndex[state]];
 
+			}
+		}
+		break;
+	case State::INIT_WORKIMM:
+		if (SoftwareSerial::available())
+		{
+			if (engine.update(read()) > 0)
+			{
+				engine.update(0);
+				char r = engine.getMessage()[0];
+				workImmediately = r == '0';
+				state = State::READY;
+				engine.prefix = statePrefixes[statePrefixIndex[state]];
+
+			}
+		}
+		break;
+	case State::READY:
+		break;
+	case State::SETTING_ROLE:
+		break;
+	case State::SETTING_NAME:
+		if (overTime)
+		{
+			updateName();
+			state = State::READY;
+			engine.prefix = statePrefixes[statePrefixIndex[state]];
+		}
+		else if (SoftwareSerial::available())
+			engine.update(read());
+		break;
 	}
-        break;
-    case State::INIT_GETROLE:
-        readBuffer();
-	if (bLoc == 8)
-	{
-	    char r = 0;
-	    if (buffer[0] == 'O' && buffer[1] == 'K' && buffer[2] == '+' &&
-	      buffer[3] == 'G' && buffer[4] == 'e' &&buffer[5] == 't' &&
-	      buffer[6] == ':')
-	    {
-	        r = buffer[7];
-	    }
-	    if (r == '0') role = Role.PERIPHERAL;
-	    else (r == '1') role = Role.CENTRAL;
-	    else role = Role.UNDEFINED;
-	    bLoc = 0;
-	    buffer[0] = 0;
-	    state = State::INIT_GETDISCNAME;
-	}
-        break;
-    case State::SETTING_NAME:
-        //
-        break;
-    }
 }
-bool PLabBTLEController::isReady() const {return state == State::READY;}
+bool PLabBTLEController::isReady() const { return state == State::READY; }
 
 void PLabBTLEController::setName(char *name) const
 {
-    if (!isReady()) return;
-    state = SETTING_NAME;
-    bLoc = 0;
-    delete[] name;
-    name = 0;
-    print("AT+NAME");
-    print(name);
-
+	if (!isReady() || connected) return;
+	state = State::SETTING_NAME;
+	delete[] name;
+	name = 0;
+	print("AT+NAME");
+	print(name);
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
 }
 const char* PLabBTLEController::getName() const
 {
-    if (state >= State::READY && state != State::SETTING_NAME)
-    {
-        return name;
-    }
-    return 0;
+	if (state >= State::READY && state != State::SETTING_NAME)
+	{
+		return name;
+	}
+	return 0;
 }
 
-Role PLabBTLEController::getRole() const {return role;}
-void PLabBTLEController::setRole(Role r);
+Role PLabBTLEController::getRole() const { return role; }
+void PLabBTLEController::setRole(Role r) {
+	if (!isReady() || r == Role::UNDEFINED || connected) return;
+	state = State::SETTING_ROLE;
+	print("AT+ROLE");
+	print((char)r);
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
 bool PLabBTLEController::isPeripheral() const
 {
-    return role == Role::PERIPHERAL;
+	return role == Role::PERIPHERAL;
 }
 bool PLabBTLEController::isCentral() const
 {
-    return role == Role::CENTRAL;
+	return role == Role::CENTRAL;
 }
 
-bool PLabBTLEController::isDiscoveringNames() const {return discoverNames;}
-void PLabBTLEController::setDiscoveringNames(bool disc);
+bool PLabBTLEController::isDiscoveringNames() const { return discoverNames; }
+void PLabBTLEController::setDiscoveringNames(bool disc)
+{
+	if (!isReady() || connected) return;
+	state = State::SETTING_NAME_DISCOVERY;
+	if (disc)
+		write("AT+SHOW1");
+	else
+		write("AT+SHOW0");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
 
-void PLabBTLEController::discoverDevices();
-bool PLabBTLEController::isDiscoveringDevices() const;
-int PLabBTLEController::devices() const;
+bool PLabBTLEController::isWorkingImmediately() const { return workImmediately; }
+void PLabBTLEController::setWorkingImmediately(bool wi)
+{
+	if (!isReady() || connected) return;
+	state = State::SETTING_WORKIMM;
+	if (wi)
+		write("AT+IMME0");
+	else
+		write("AT+IMME1");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
 
-void PLabBTLEController::connectDevice(int id);
-void PLabBTLEController::connectDevice(const PLabBTLEDevice &device);
-void PLabBTLEController::connectLastDevice();
-bool PLabBTLEController::connectionFailed() const;
-bool PLabBTLEController::isConnecting() const;
-bool PLabBTLEController::isConnected() const;
-void PLabBTLEController::disconnect();
+void PLabBTLEController::discoverDevices()
+{
+	if (!isReady() || connected || role != Role::CENTRAL || workImmediately) return;
+	state = State::DISCOVERY_RUNNING;
+	write("AT+DISC?");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+	for (int i = 0; i < PLAB_BTLE_MAXDEVICE; ++i) _devices[i].id = -1;
+}
+bool PLabBTLEController::isDiscoveringDevices() const { return state == State::DISCOVERY_RUNNING; }
+int PLabBTLEController::devices() const
+{
+	for (int i = 0; i < PLAB_BTLE_MAXDEVICE; ++i)
+	{
+		if (_devices[i].id < 0) return i;
+	}
+	return PLAB_BTLE_MAXDEVICE;
+}
 
-void PLabBTLEController::reset() const;
-void PLabBTLEController::factoryReset();
+void PLabBTLEController::connectDevice(int id)
+{
+	if (!isReady() || connected || role != Role::CENTRAL || workImmediately) return;
+	if (id >= devices()) return;
+	state = State::CONNECT_RUNNING;
+	print("AT+CONN");
+	print(id);
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
+void PLabBTLEController::connectDevice(const PLabBTLEDevice &device)
+{
+	if (!isReady() || connected || role != Role::CENTRAL || workImmediately || device.id < 0) return;
+	state = State::CONNECT_RUNNING;
+	print("AT+CONN");
+	print(device.id);
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
+void PLabBTLEController::connectLastDevice()
+{
+	if (!isReady() || connected || role != Role::CENTRAL || workImmediately) return;
+	state = State::CONNECT_RUNNING;
+	write("AT+CONNL");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
+bool PLabBTLEController::connectionFailed() const { return connFail; }
+bool PLabBTLEController::isConnecting() const { return state == State::CONNECT_RUNNING; }
+bool PLabBTLEController::isConnected() const { return connected; }
+void PLabBTLEController::disconnect()
+{
+	if (!isReady() || !connected || role != Role::CENTRAL) return;
+	state = State::DISCONNECTING;
+	write("AT");
+	connected = false;
+	connFail = false;
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
+
+void PLabBTLEController::reset() const
+{
+	if (!isReady() || connected) return;
+	state = State::RESET_IN_PROGRESS;
+	write("AT+RESET");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
+void PLabBTLEController::factoryReset()
+{
+	if (!isReady() || connected) return;
+	state = State::FACTORY_RESET_IN_PROGRESS;
+	write("AT+RENEW");
+	engine.prefix = statePrefixes[statePrefixIndex[state]];
+}
--- PLabBT/PLabBTLEController.h
+++ PLabBT/PLabBTLEController.h
@@ -3,7 +3,7 @@
  * Version 0.1, June, 2015
  * This library for the Arduino Uno simplify network communication for
  * Bluetooth LE HM-10 and HM11 based devices. This library extends
- * the SoftwareSerial library, and wrap a controller around AT commands. 
+ * the SoftwareSerial library, and wrap a controller around AT commands.
  *
  * Created by Inge Edward Halsaunet, 2015
  * Released into the public domain
@@ -28,99 +28,175 @@
 #include <SoftwareSerial.h>
 
 class PLabBTLEDevice {
-friend class PLabBTLEController;
+	friend class PLabBTLEController;
 private:
-    int id = -1;
-    char *name = 0;
-    char *address = 0;
+	int id = -1;
+	char *name = 0;
+	char *address = 0;
 public:
-    ~PLabBTLEDevice()
-    {
-       delete[] name;
-       delete[] address;
-    }
-    int id() const;
-    const char *name() const;
-    const char *address() const;
-}
+	~PLabBTLEDevice()
+	{
+		delete[] name;
+		delete[] address;
+	}
+	int id() const;
+	const char *name() const;
+	const char *address() const;
+};
+
+class PLabBTLEStateEngine
+{
+private:
+	enum State {
+		START, RECV_PREFIX, RECV_MSG
+	};
+	State state = State::Start;
+	char buffer[PLAB_BTLE_BSIZE] = { 0 };
+	int bLoc = 0;
+	int pLoc = 0;
+public:
+	char *prefix = 0;
+	// -1 : ERROR
+	//  0 : Receiving prefix
+	//  1 : Receiving message
+	// On end-of-message recive (0), return length of message
+	int update(char c) {
+		switch (state)
+		{
+		case State::START:
+			bLoc = 0;
+			buffer[bLoc] = 0;
+			if (prefix == 0)
+			{
+				state = State::RECV_MSG;
+			}
+			else
+			{
+				pLoc = 0;
+				return prefix[pLoc++] == c ? 0 : -1;
+			}
+			break;
+		case State::RECV_PREFIX:
+			if (prefix[pLoc] == 0)
+			{
+				state = State::RECV_MSG;
+			}
+			else
+			{
+				return prefix[pLoc++] == c ? 0 : -1;
+			}
+			break;
+		}
+		// If we get here, we are in state RECV_MSG
+		if (c == 0)
+		{
+			state = State::Start;
+			return bLoc;
+		}
+		if (bLoc >= (PLAB_BTLE_BSIZE - 1))
+		{
+			buffer[PLAB_BTLE_BSIZE - 1] = 0;
+			return -1;
+		}
+		buffer[bLoc++] = c;
+		buffer[bLoc] = 0;
+		return 1;
+	}
+	char *getMessage() { return buffer; }
+};
 
 class PLabBTLEController : public SoftwareSerial
 {
 private:
-    enum State {
-        INIT, INIT_GETNAME, INIT_GETROLE, INIT_GETDISCNAME,
-	READY,
-	SETTING_ROLE, SETTING_NAME, SETTING_NAME_DISCOVERY,
-	DISCOVERY_RUNNING, CONNECT_RUNNING, DISCONNECTING,
-	RESET_IN_PROGRESS, FACTORY_RESET_IN_PROGRESS
-    };
+	enum State {
+		INIT = 0, INIT_GETNAME, INIT_GETROLE, INIT_GETDISCNAME, INIT_WORKIMM,
+		READY,
+		SETTING_ROLE, SETTING_NAME, SETTING_NAME_DISCOVERY, SETTING_WORKIMM,
+		DISCOVERY_RUNNING, CONNECT_RUNNING, DISCONNECTING,
+		RESET_IN_PROGRESS, FACTORY_RESET_IN_PROGRESS
+	};
+	const char *statePrefixes = {
+		0, "OK+NAME:", "OK+Get:", "OK+Set:", "OK+DIS", "OK+RESET", "OK+RENEW",
+		"OK+CONN"
+	};
+	const int statePrefixIndex[] = {
+		0, 1, 2, 2, 2,
+		0,
+		3, 3, 3, 3,
+		4, 7, 0,
+		5, 6
+	};
 
-    state = State::INIT;
+	PLabBTLEStateEngine engine;
 
-    char buffer[PLAB_BTLE_BSIZE];
-    int bLoc = 0;
-    unsigned long lastTime;
+	state = State::INIT;
 
-    char *name = 0;
-    PLabBTLEDevice *devices;
-    bool connected, discoverNames;
-    Role role = Role::UNDEFINED;
-    // Defaultconstructor should never be used
-    PLabBTLEController() : SoftwareSerial(0,1){};
-    void init(long speed);
-    void readBuffer();
+	unsigned long lastTime;
+
+	char *name = 0;
+	PLabBTLEDevice *_devices;
+	bool connected, discoverNames, workImmediately, connFail = false;;
+	int connectedTo = -1;
+	Role role = Role::UNDEFINED;
+	// Defaultconstructor should never be used
+	PLabBTLEController() : SoftwareSerial(0, 1){};
+	void init(long speed);
+	void updateName();
 public:
 
-    enum Role {UNDEFINED=0, PERIPHERAL='0', CENTRAL='1'};
+	enum Role { UNDEFINED = 0, PERIPHERAL = '0', CENTRAL = '1' };
 
-    PLabBTLEController(int rx, int tx, long speed = 9600) :
-      SoftwareSerial(rx,tx) {
-	init(speed);
-    }
-    PLabBTLEController(const PLabBTLEController &other) = delete;
-    ~PLabBTLEController() : ~SofwareSerial() {
-        delete[] name;
-	delete[] devices;
-    }
-
-    const PLabBTLEDevice &operator[](const int &index) const;
-
-    void update();
-    bool isReady() const;
-
-    void setName(char *name) const;
-    const char* getName() const;
-
-    Role getRole() const;
-    void setRole(Role r);
-    bool isPeripheral() const;
-    bool isCentral() const;
-
-    bool isDiscoveringNames() const;
-    void setDiscoveringNames(bool disc);
-
-    void discoverDevices();
-    bool isDiscoveringDevices() const;
-    int devices() const;
-
-    void connectDevice(int id);
-    void connectDevice(const PLabBTLEDevice &device);
-    void connectLastDevice();
-    bool connectionFailed() const;
-    bool isConnecting() const;
-    bool isConnected() const;
-    void disconnect();
-
-    virtual int available()
-    {
-        if (state == State::READY)
-	{
-	    return SofwareSerial::available();
+	PLabBTLEController(int rx, int tx, long speed = 9600) :
+		SoftwareSerial(rx, tx) {
+		init(speed);
 	}
-	return 0;
-    }
+	PLabBTLEController(const PLabBTLEController &other) = delete;
+	~PLabBTLEController() : ~SofwareSerial() {
+		delete[] name;
+		delete[] _devices;
+	}
 
-    void reset() const;
-    void factoryReset();
+	const PLabBTLEDevice &operator[](const int &index) const;
+
+	void update();
+	bool isReady() const;
+
+	void setName(char *name) const;
+	const char* getName() const;
+
+	Role getRole() const;
+	void setRole(Role r);
+	bool isPeripheral() const;
+	bool isCentral() const;
+
+	bool isDiscoveringNames() const;
+	void setDiscoveringNames(bool disc);
+
+	bool isWorkingImmediately() const;
+	void setWorkingImmediately(bool wi);
+
+	void discoverDevices();
+	bool isDiscoveringDevices() const;
+	int devices() const;
+
+	void connectDevice(int id);
+	void connectDevice(const PLabBTLEDevice &device);
+	void connectLastDevice();
+	bool connectionFailed() const;
+	bool isConnecting() const;
+	bool isConnected() const;
+	void disconnect();
+
+	virtual int available()
+	{
+		if (state == State::READY)
+		{
+			return SofwareSerial::available();
+		}
+		return 0;
+	}
+
+	void reset() const;
+	void factoryReset();
 }
 #endif
